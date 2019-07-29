361827e55cec36fcc7567ec45328aac6ac8bc978
SuperMario210 <Sup3rMar1o210@gmail.com>
2019-07-29 14:04:33 -0700

    actually fixed it this time

--- cloud/util/utilFunctions.js
+++ cloud/util/utilFunctions.js
@@ -114,7 +114,7 @@ module.exports = {
     const likeQuery = new Parse.Query(parseObject.Like);
     likeQuery.equalTo("entry", entry);
     likeQuery.equalTo("user", user);
-    return await likeQuery.first() == null;
+    return await likeQuery.first() != null;
   },
 
   /**
